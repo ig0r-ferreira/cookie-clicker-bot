9920251a4cf749143e9e94095a93f8f7f9805c35
Igor Ferreira <iferreira.n01@gmail.com>
2022-12-26 23:21:32 -0300

    Add handling for bot interrupt

--- cookie_clicker_bot/__main__.py
+++ cookie_clicker_bot/__main__.py
@@ -1,12 +1,16 @@
 from datetime import timedelta
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchWindowException
 
 from cookie_clicker_bot.bot import CookieBot
 
 
 def main() -> None:
-    CookieBot(webdriver.Chrome()).run(timedelta(minutes=1))
+    try:
+        CookieBot(webdriver.Chrome()).run(timedelta(minutes=1))
+    except NoSuchWindowException:
+        print('Bot execution stopped, program will exit.')
 
 
 if __name__ == '__main__':
