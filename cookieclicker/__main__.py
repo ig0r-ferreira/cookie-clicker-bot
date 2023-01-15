f357d06209fd1c5f22c5e02e668fe1dd73229b61
Igor Ferreira <iferreira.n01@gmail.com>
2023-01-15 09:00:39 -0300

    Add CLI args

--- cookieclicker/__main__.py
+++ cookieclicker/__main__.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser, ArgumentTypeError
 from datetime import timedelta
 
 from selenium import webdriver
@@ -5,9 +6,37 @@ from selenium import webdriver
 from cookieclicker.bot import CookieBot
 
 
+def value_greater_than_zero(string) -> float:
+    try:
+        value = float(string)
+    except ValueError:
+        raise ArgumentTypeError(f'{string!a} is not a number.')
+    else:
+        if value <= 0:
+            raise ArgumentTypeError(f'{string!a} is not greater than zero.')
+        return value
+
+
 def main() -> None:
+    parser = ArgumentParser(
+        description='Auto click bot for Cookie Clicker online game.'
+    )
+    parser.add_argument(
+        'duration', help='Duration in minutes.', type=value_greater_than_zero
+    )
+    parser.add_argument(
+        '-delay',
+        help='Delay until the next purchase of items. Default is %(default)d.',
+        type=value_greater_than_zero,
+        default=3,
+    )
+    args = vars(parser.parse_args())
+
     cookie_bot = CookieBot(webdriver.Chrome())
-    cookie_bot.run(timedelta(minutes=1))
+    cookie_bot.run(
+        duration=timedelta(minutes=args['duration']),
+        upgrade_delay=args['delay'],
+    )
     print(cookie_bot.get_performance_info())
 
 
